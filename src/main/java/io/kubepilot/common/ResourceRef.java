c285d4b8a2ee00f6671fa77d88de97383bd57175
Gous0786 <gousalam0786@gmail.com>
2026-08-12 15:13:03 +0530

    Complete ResourceRef record definition

--- src/main/java/io/kubepilot/common/ResourceRef.java
+++ src/main/java/io/kubepilot/common/ResourceRef.java
@@ -1,6 +1,18 @@
 package io.kubepilot.common;
 
-public record ResourceRef {
+public record ResourceRef(String kind, String namespace, String name, String container) {
 
-    
+    public static ResourceRef of(String kind, String namespace, String name) {
+        return new ResourceRef(kind, namespace, name, null);
+    }
+
+    public ResourceRef withContainer(String container) {
+        return new ResourceRef(kind, namespace, name, container);
+    }
+
+    @Override
+    public String toString() {
+        String base = kind + "/" + namespace + "/" + name;
+        return container == null ? base : base + "[" + container + "]";
+    }
 }
